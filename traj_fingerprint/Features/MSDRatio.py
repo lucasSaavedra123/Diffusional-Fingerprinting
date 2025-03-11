f67ae73c95b05973badb4d4d3dc4349338f4a5dc
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2025-03-11 15:53:42 -0300

    Features take cache info from traj

--- traj_fingerprint/Features/MSDRatio.py
+++ traj_fingerprint/Features/MSDRatio.py
@@ -5,16 +5,4 @@ from .Feature import Feature
 
 class MSDRatio(Feature):
     def calculate(self, trajectory):
-        DELTA_T = 0.000132
-        TIME_START = 0.000084
-        MAX_T = 0.050
-
-        _, msd, _, _, _, _ = trajectory.temporal_average_mean_squared_displacement(
-            log_log_fit_limit=MAX_T,
-            limit_type='time',
-            bin_width=DELTA_T,
-            time_start=TIME_START,
-            with_corrections=True
-        )
-
-        return [MSDratio(msd)]
+        return [MSDratio(trajectory.info['msd'])]
--- traj_fingerprint/Features/MeanMSD.py
+++ traj_fingerprint/Features/MeanMSD.py
@@ -5,16 +5,4 @@ from .Feature import Feature
 
 class MeanMSD(Feature):
     def calculate(self, trajectory):
-        DELTA_T = 0.000132
-        TIME_START = 0.000084
-        MAX_T = 0.050
-
-        _, msd, _, _, _, _ = trajectory.temporal_average_mean_squared_displacement(
-            log_log_fit_limit=MAX_T,
-            limit_type='time',
-            bin_width=DELTA_T,
-            time_start=TIME_START,
-            with_corrections=True
-        )
-
-        return [np.mean(msd)]
+        return [np.mean(trajectory.info['msd'])]
