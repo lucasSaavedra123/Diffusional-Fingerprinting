5d900a6a6e762d6292542229443504128066f055
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2025-03-18 15:14:53 -0300

    Displacements updated

--- traj_fingerprint/Features/Displacements.py
+++ traj_fingerprint/Features/Displacements.py
@@ -4,5 +4,15 @@ from .Feature import Feature
 
 
 class Displacements(Feature):
+    """
+    Obtained from https://doi.org/10.21203/rs.3.rs-3716053/v1
+    """
     def calculate(self, trajectory):
-        return [np.mean(trajectory.displacements())]
+        displacements = trajectory.displacements()
+        return [
+            np.mean(displacements),
+            np.std(displacements),
+            np.min(displacements),
+            np.max(displacements),
+            np.std(displacements)/np.mean(displacements)
+        ]
