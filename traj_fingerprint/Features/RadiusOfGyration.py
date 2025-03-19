e053c525885bb4f4f452e79047d1b614a2a030e4
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2025-03-19 10:51:43 -0300

    Comment added

--- traj_fingerprint/Features/RadiusOfGyration.py
+++ traj_fingerprint/Features/RadiusOfGyration.py
@@ -6,6 +6,8 @@ from .Feature import Feature
 class RadiusOfGyration(Feature):
     """
     Obtained from https://doi.org/10.3390/receptors4010006
+    However, I found other definition by (Wagner, 2017) which
+    works with eigenvalues. Check which definition to use.
     """
     def calculate(self, trajectory):
         x,y = trajectory.get_noisy_x(), trajectory.get_noisy_y()
