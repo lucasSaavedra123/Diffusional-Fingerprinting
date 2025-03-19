03392017cc0610c65435d23539da0eb62e546e2f
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2025-03-19 13:18:29 -0300

    Fix

--- traj_fingerprint/Features/VelocityAutocorrelation.py
+++ traj_fingerprint/Features/VelocityAutocorrelation.py
@@ -5,13 +5,13 @@ from .Feature import Feature
 
 def empirical_velocitiy_autocorrelation(n, raw_t):
     dots = []
-    for i in range(0, len(raw_t)-2):
-        t_i_1 = raw_t[i+1]
-        t_i_1_n = raw_t[i+1+n]
-        t_i = raw_t[i]
-        t_i_n = raw_t[i+n]
-
-        dots.append(np.linalg.norm(t_i_1_n-t_i_n) * np.linalg.norm(t_i_1-t_i))
+    for i in range(0, len(raw_t)):
+        if i+1+n < len(raw_t):
+            t_i_1 = raw_t[i+1]
+            t_i_1_n = raw_t[i+1+n]
+            t_i = raw_t[i]
+            t_i_n = raw_t[i+n]
+            dots.append(np.linalg.norm(t_i_1_n-t_i_n) * np.linalg.norm(t_i_1-t_i))
 
     return np.sum(dots)/(len(raw_t)-1)
 
