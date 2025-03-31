65582b5947a0da439a96d99b2ff451a056c80947
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2025-03-31 14:43:58 -0300

    ConvexDensityOfPoints added

---
+++ traj_fingerprint/Features/ConvexDensityOfPoints.py
@@ -0,0 +1,18 @@
+from scipy.spatial import ConvexHull
+
+from .Feature import Feature
+
+
+class ConvexDensityOfPoints(Feature):
+    @property
+    def names(self):
+        return ['ConvexDensityOfPoints']
+
+    """
+    Obtained from (Vogler, 2023).
+    """
+    def calculate(self, trajectory):
+        n_rho = trajectory.length
+        a_convex = ConvexHull(trajectory.raw_trajectory).volume
+        density = n_rho / a_convex
+        return [density]
